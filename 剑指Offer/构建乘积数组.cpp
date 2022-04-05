79dc36c5247b3ae16ae1c00c18f1cf974979cbe7
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-05 21:11:29 +0800

    Update 构建乘积数组.cpp
    其实不是非常清楚怎么回事，大概知道

--- 剑指Offer/构建乘积数组.cpp
+++ 剑指Offer/构建乘积数组.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> multiply(const vector<int>& A) {
-        // 用的最傻瓜的方式
-        // 主要就是用了 j %= A.size(); 来循环索引A的值
-        // 结果只超过了1%的人
-        vector<int> B;
-        for(int i = 0;i<A.size();i++)
+        // 注意还是要多多体会一下感觉，目前我脑子里不是很清楚明了
+        // 确实不可以用 累乘/A[i] 来计算，因为很可能A[i]=0;
+        // 这个用的是前缀乘积 乘以 后缀乘积的方式来实现
+        // 需要注意体会空间复杂度的降低，直接用B来代替left[i]以及用temp来替代right[i]
+        vector<int> B(A.size(),1);
+        for(int i=1;i<A.size();++i)
         {
-            int temp = 1;
-            for(int j = (i+1)%A.size(),cnt = 0;cnt<A.size()-1;++j,++cnt) 
-            {
-                j %= A.size();
-                temp *= A[j];
-            }
-            B.push_back(temp);
+            B[i] = B[i-1] * A[i-1];   // left[i]这里用B[i]替代，减少空间复杂度
+        }
+        int temp = 1;
+        for(int j=A.size()-2;j>=0;--j)
+        {
+            temp = temp*A[j+1];        // right[i]用temp代替
+            B[j] = B[j] * temp;        // 就是一次一次累乘，节约空间
         }
         return B;
     }
