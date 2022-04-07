76adb114fc9842a69e716b8fcef027d18df29aa9
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-07 15:26:42 +0800

    Update 扑克牌顺子.cpp
    代码结构比我的好，好很多

--- 剑指Offer/扑克牌顺子.cpp
+++ 剑指Offer/扑克牌顺子.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     bool IsContinuous( vector<int> numbers ) {
-        // 纯粹我自己写的哈，思路主要是
-        // 先排序，当第一个不为零的数与末尾的数的差值等于对应索引的差值，肯定是连续，因为0是任意数
-        // 或者当零的数量可以补足不为零的第一个数与末尾数的差值与索引差值的差值，也可以补成连续的
-        // 注意的是，如果不为零的数有重复，直接返回false
-        // 另外，如果第一个不为零的数不满足上述条件，可以直接返回false
-        sort(numbers.begin(),numbers.end()); // 先排序
-
-        for(int i=0;i<numbers.size();++i)
-        {
-            if(numbers[i] != 0) 
-            {
-                for(int j = i;j<numbers.size();++j)   // 如果不为零的数有重复，直接返回false
-                    for(int k=j+1;k<numbers.size();++k)
-                    {
-                        if(numbers[j] == numbers[k] ) return false;
-                    }
-
-                if(numbers.size()-1-i == numbers[numbers.size()-1] - numbers[i]) return true;
-                if(numbers.size()-1 == numbers[numbers.size()-1] - numbers[i]) return true;
-                
-                return false;
-            }
+        // 这是别人的代码，思路是一样的，但是实现好像简洁一些
+        // 主要就是代码结构安排比较好
+        // interval的想法比较好
+        
+        /*
+            顺子牌的特点：
+            1、顺子一定没有相等的牌；
+            2、顺子中两张相邻的扑克牌的数值差为1，即满足interrapt=numbers[i + 1] - numbers[i] - 1==0;
+            3、当interrapt不为0，代表需要在顺子中插入对应interrapt张牌；
+            4、只有两张王牌；
+            主要思路：
+            1、排序五张牌；
+            2、遍历5张牌，并求出大小王的数量zero_num，与interrapt的大小；
+            3、判断interrapt的大小。
+            (1)interrapt==0,除大小王之外的牌本来就是顺子，大小王随便补齐在头部或者尾部；
+            (2)interrapt<=zero_num,除大小王之外的牌不是顺子，可以通过大小王变成特定的牌补在其中，使其成为顺子；
+            (2)interrapt>zero_num,除大小王之外的牌不是顺子,即使有大小王也补不成顺子。
+        */
+        
+        sort(numbers.begin(), numbers.end());
+        int zero_num = 0;//统计大小王数量
+        int i = 0;
+        while (numbers[i] == 0)zero_num++,i++;
+        int interrapt = 0;//记录五张牌中最大值max到最小值min的距离
+        for (; i < numbers.size()-1; ++i) {
+            if (numbers[i] == numbers[i + 1])return false;//出现相同的扑克牌
+            interrapt += numbers[i + 1] - numbers[i] - 1;//计算距离
         }
+        if (zero_num >= interrapt) return true;
         return false;
     }
 };
