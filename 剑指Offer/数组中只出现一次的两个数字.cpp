20ff3e8ea3a2df9ec8caa43f2705cb55d7130922
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-09 11:16:24 +0800

    Update 数组中只出现一次的两个数字.cpp
    异或、&1分类

--- 剑指Offer/数组中只出现一次的两个数字.cpp
+++ 剑指Offer/数组中只出现一次的两个数字.cpp
@@ -9,18 +9,61 @@ public:
      */
     vector<int> FindNumsAppearOnce(vector<int>& array) {
         // write code here
-        // 我自己写的代码，vector存，如果遇到重复，就剔除，无重复就继续push_back
-        sort(array.begin(),array.end());
-        vector<int> stk;
-        for(int i=0;i<array.size();++i)
+        
+        // 学习的别人的想法
+        
+        // 使用 异或 的性质
+          // （0） 只有在两个比较的位不同时其结果是1，否则结果为0
+　　      //（1） 0^0=0，0^1=1 0异或任何数＝任何数
+　　      //（2） 1^0=1，1^1=0 1异或任何数=任何数取反
+　　      //（3） 任何数异或自己＝把自己置0
+        
+        // 我们要想对两者进行分组操作，就是需要找到两者中的那一位不同的二进制，然后得到分组的与值（去&的那个值）
+        // &1 就是按最低位分奇偶 &0010按倒数第二位 &0100同理可推为倒数第三位
+        
+        // 先将全部数进行异或运算，得出最终结果 为两个不同数的异或结果
+        int tmp = 0;
+        for(auto num:array)
         {
-            if(stk.empty()) stk.push_back(array[i]);  // 数组空的时候，只需要添加就行
-            else if(array[i]!=stk[stk.size()-1])        // 如果下一个不等于上一个，就存进去
-            {
-                stk.push_back(array[i]);
-            }
-            else stk.resize(stk.size()-1);       // 如果相等，就让size-1，相当于删了
+            tmp^=num;
         }
-        return stk;
+        
+        // 找到那个可以充当分组去进行与运算的数
+        // 从最低位开始找起 mask=1 即0001;
+        
+        int mask = 1;
+        while((tmp&mask)==0) mask<<=1;
+        
+        // 进行分组，分成两组，转换为两组 求出现一次的数字 去求
+        int a=0;
+        int b=0;
+        for(auto num:array)
+        {
+            if((num&mask)==0)  // 这里只能判别==0哈，因为不同为0000，但是相同的话，结果不定
+            {
+                a^=num;
+            }
+            else
+            {
+                b^=num;
+            }
+        }
+        
+        // 因为题目要求小的数放前面，所以这一做个判断
+        vector<int> res;
+        res.resize(2);
+        if(a>b)
+        {
+            res[0] = b;
+            res[1] = a;
+        }
+        else
+        {
+            res[0] = a;
+            res[1] = b;
+        }
+        return res;
+        
+        
     }
 };
