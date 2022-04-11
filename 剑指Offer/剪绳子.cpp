0808cd4f5c1623cea233e7543c39f5c9cf27b9a7
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-11 12:07:44 +0800

    Update 剪绳子.cpp
    感觉我根本看不懂。。。很难理解，但其实想法挺简单的啊

--- 剑指Offer/剪绳子.cpp
+++ 剑指Offer/剪绳子.cpp
@@ -7,35 +7,16 @@ public:
      * @param n int整型 
      * @return int整型
      */
-    int back_track(int n,vector<int> &mark)
-    {
-        // n<=4
-        if(n<=4) return n;
-        
-        // 记忆化搜索
-        if(mark[n] != -1)
-        {
-            return mark[n];
-        }
-        
-        int ret = 0;
-        for(int i=1;i<n;++i)
-        {
-            ret = max(ret,i*back_track(n-i,mark));
-        }
-        
-        // 添加进mark啊
-        return mark[n] = ret;
-    }
     
     int cutRope(int n) {
         // write code here
-        if(n == 2) return 1;
-        if(n == 3) return 2;
-        
-        // 记忆化搜索部分
-        vector<int> mark(n+1,-1);
-        return back_track(n,mark);
-        
+        if(n == 2 || n == 3) return n-1;
+        int res = 1;
+        while(n>4)
+        {
+            n-=3;
+            res *=3;
+        }
+        return n*res;
     }
 };
