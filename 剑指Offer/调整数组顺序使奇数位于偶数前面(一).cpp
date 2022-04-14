6f0120e487c045954189dabf7eb1e9bdf4828d02
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-14 17:25:31 +0800

    Update 调整数组顺序使奇数位于偶数前面(一).cpp
    超越60%多的提交。。。厉害了

--- 剑指Offer/调整数组顺序使奇数位于偶数前面(一).cpp
+++ 剑指Offer/调整数组顺序使奇数位于偶数前面(一).cpp
@@ -7,16 +7,14 @@ public:
      * @param array int整型vector 
      * @return int整型vector
      */
-    vector<int> reOrderArray(vector<int>& array) {
+        vector<int> reOrderArray(vector<int>& array) {
         // write code here
-        // 最简单的方法，我一开始没用这个，一开始用的新建一个数组，但是偶数的顺序不变好像需要stack
-        vector<int> odd,even;
-        for(auto k:array)
-        {
-            if(k&1) odd.push_back(k);
-            else even.push_back(k);
-        }
-        odd.insert(odd.end(),even.begin(),even.end());
-        return odd;
+        // 属于是骚操作了！！！值得学习！
+        // STL+Lambda
+        stable_partition(array.begin(), array.end(),[](int x){
+            return x&1;    // 匿名函数实现筛选规则
+        });
+        return array;
     }
+
 };
