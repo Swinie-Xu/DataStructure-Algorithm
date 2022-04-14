5640c6702ed2f573bff2b4bf93f732a6d0a9afee
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-14 16:36:40 +0800

    Update 孩子们的游戏(圆圈中最后剩下的数).cpp
    约瑟夫环，递归和迭代版

--- 剑指Offer/孩子们的游戏(圆圈中最后剩下的数).cpp
+++ 剑指Offer/孩子们的游戏(圆圈中最后剩下的数).cpp
@@ -1,23 +1,40 @@
 class Solution {
 public:
     int LastRemaining_Solution(int n, int m) {
-        // 我的想法是构建一个循环链表，再每次去剔除,可以用数组来实现循环链表啊！！
-        // 当时脑子浆糊了，没想到每次报数的mm是和m相关的，m-1
-        // 后来好几天以后，一下子发现了！！！
-        vector<int> vec(n,0);
-        int cnt=0; // 记录扔出去的个数，n-1个就停
-        int mm=0;    // 每一轮的报数
-        for(int i=0;cnt!=n-1;i = (++i)%n)
-        {
-            
-            if(vec[i]==-1) continue;
-            else if(mm==m-1) vec[i] = -1, cnt++, mm=0;  // 到了报数的人，就出列（置为-1）并计数，并重置计数器
-            else ++mm;
-        }
-        for(int i=0;i<n;++i)
-        {
-            if(vec[i]!=-1) return i;
-        }
-        return -1;
+        // 所谓约瑟夫环，可是递推过程我没看懂
+        // 记住？？？
+        
+        /*
+            公式递推：
+            令f(n,m)表示最后一个人的下标。
+            1.假设有n个人，报数m， 从0 开始报数，易知出圈的人下标为 m-1。
+            2.m-1 出圈后，我们对剩余人重新编号 即 第m个人下标为0，第m+1 下标为1 ......以此编号。 那么重新编号之后，那么最后一个人的下标为f(n-1,m)
+            问题1： 在重新编号之后的f(n-1,m) 与 重新编号之前的f(n,m)有什么关系？
+            重新编号之前 m, m+1,m+2,....
+            重新编号之后 0 ，1 ，2，....
+            可知 (新编号+m)%n = 旧编号
+            3. f(n,m) = (f(n-1,m)+m) %n;
+        */
+        
+        if(n<1||m<1) return -1;
+        int last = 0;
+        for(int i=2;i<=n;++i)
+            last = (last+m)%i;
+        return last;
+        
+        
+        /*
+        // 递归的版本，这个厉害了啊
+        
+        class Solution {
+        public:
+            //约瑟夫
+            int LastRemaining_Solution(int n, int m) {
+                if(n <= 0) return -1;
+                return (LastRemaining_Solution(n-1,m)+m)%n;
+            }
+        };
+        */
+
     }
 };
