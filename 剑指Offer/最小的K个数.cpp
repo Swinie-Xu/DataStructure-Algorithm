72474d889327a57d9a206bc12dc1102bac91343a
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-16 20:01:06 +0800

    Update 最小的K个数.cpp
    抄的，不过学习了优先队列这个STL(大顶堆)

--- 剑指Offer/最小的K个数.cpp
+++ 剑指Offer/最小的K个数.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
-    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
-        // 这个是真的垃圾，为啥性能这么垃圾
-        // 我自己写的
-        // 而且我不知道这题考察的啥东西。。。
-        vector<int> ret;
-        if( input.empty() || k==0 ) return ret;
-        sort(input.begin(),input.end());
-        ret.assign(input.begin(),input.begin()+k);
-        return ret;
-    }
+  priority_queue<int> q;
+  vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+      // 将数组里面的数字都放入一个优先队列里面
+      // 抄的代码，大顶堆就是优先队列（默认从小到大）
+      for(auto x:input){
+          q.push(x);
+          if(q.size()>k){
+              q.pop();
+          }
+      }
+      // 对input数组
+      input.clear();
+      // 如果达不到k个数字
+      if(q.size()<k){  
+          return input;
+      }
+      while(!q.empty()){
+          input.push_back(q.top());
+          q.pop();
+      }
+      return input;
+  }
 };
