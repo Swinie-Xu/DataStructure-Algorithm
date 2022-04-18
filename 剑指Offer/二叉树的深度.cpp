4dfe31021ca9ff6298cf04418f806292c48d8ba7
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-18 21:47:34 +0800

    Update 二叉树的深度.cpp
    BFS的思想，也挺不错的啊，以前看算法图解的时候好像见过

--- 剑指Offer/二叉树的深度.cpp
+++ 剑指Offer/二叉树的深度.cpp
@@ -10,14 +10,26 @@ struct TreeNode {
 class Solution {
 public:
     int TreeDepth(TreeNode* pRoot) {
-        /*
-         函数是求二叉树的最大深度，我们不必管函数具体是怎么实现的。 
-         所以最终结果为 max( 头结点左子树的最大深度, 头结点右子树的最大深度)+1
-         */
-        
+        // 层序遍历BFS实现
+        // 广度优先遍历，就是说，一层一层的遍历
+        // https://blog.nowcoder.net/n/649bd2d6be1a4963894ef055282795d5?f=comment
         if(!pRoot) return 0;
-        int lval = TreeDepth(pRoot->left);
-        int rval = TreeDepth(pRoot->right);
-        return max(lval,rval) + 1;
+        queue<TreeNode*> q;
+        q.push(pRoot);    // 根节点入队
+        int hight = 0;    // 记录深度
+        while(!q.empty())    // 当队列不为空的时候
+        {
+            int size = q.size();    // 当前层的节点数
+            while(size != 0)    // 将当前层的节点的左右子节点都入队
+            {
+                TreeNode * node = q.front();    // 出队
+                q.pop();
+                if(node->left) q.push(node->left);    // 有左节点的时候
+                if(node->right) q.push(node->right);    // 有右节点的时候
+                size--;    // 出一个
+            }
+            hight++;    // 里层循环结束则证明一走完一层，所以深度加1
+        }
+        return hight;
     }
 };
