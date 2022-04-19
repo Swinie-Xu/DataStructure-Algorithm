01eceedec94c4a1e553a39e2219b33a12e086092
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-19 21:07:32 +0800

    Update 链表中环的入口结点.cpp
    链表找环，用快慢指针很快确定，但是要找到环的入口，还需要点功夫

--- 剑指Offer/链表中环的入口结点.cpp
+++ 剑指Offer/链表中环的入口结点.cpp
@@ -10,13 +10,28 @@ struct ListNode {
 class Solution {
 public:
     ListNode* EntryNodeOfLoop(ListNode* pHead) {
-        unordered_set<ListNode*> st;  // 哈希集合
-        while(pHead)
+        // https://blog.nowcoder.net/n/4c6d12e823344a42a51164a127208265?f=comment
+        // 快慢指针，可以很同意判断是否有环
+        // 快指针每次走两步，慢指针每次走一步，那么若进入环中，每次他们之间的相对距离都会-1，直到两者相遇。
+        // 可知，两指针相遇时，慢指针已经走了nb步，已知我们要走到入口节点，需要走a + kb步，而这时s = nb只要再走a即可到达入口，我们把快指针移动到头节点，然后两个指针一步一步往后走，当它们相遇时所处的位置就是入口节点
+        
+        ListNode* fast = pHead,*slow = pHead;    // 快慢指针一开始都指向头
+        while(fast) 
         {
-            if(st.count(pHead))return pHead; // 若已经记录过，直接返回
-            st.insert(pHead);// 记录当前结点
-            pHead = pHead->next;
+            slow = slow->next;    // 慢指针走一步
+            if(fast->next == nullptr) return nullptr;    // 若快指针的下一步不能走，则说明两指针不会相遇
+            fast = fast->next->next;    // 快指针向后走两步
+            if(fast == slow)    // 找到相交节点, 此时慢指针已经走了nb步
+            {
+                fast = pHead;    // 快指针重新移动到头
+                while(fast!=slow)    // 直到两指针相遇位置，每次向后走一步
+                {
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return fast;    // 找到入口节点，直接返回
+            }
         }
-        return nullptr;// 无环
+        return nullptr;
     }
 };
