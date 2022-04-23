787f4272e4f0d4aeb69bbf9fbdee237c82d4f165
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-23 23:55:11 +0800

    Update 矩形覆盖.cpp
    记忆化搜搜，但是自己写的总是出错，改成了参考答案的数组方式

--- 剑指Offer/矩形覆盖.cpp
+++ 剑指Offer/矩形覆盖.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
+    int mem[1000];
     int rectCover(int number) {
-        // 这个就是斐波那契数列，不知道为啥
-        if(number < 1) return 0;
-        else if(number<=2) return number;
-        else return rectCover(number-1) + rectCover(number-2);
+        // 记忆化搜索的斐波那契数列
+        // 自己写了半天没有通过，服了！
+        if(mem[number]) return mem[number];
+        if(number < 1) 
+        {
+            return 0;
+        }
+        if(number<=2) 
+        {
+            mem[number] = number;
+        }
+        else {
+            mem[number] = rectCover(number-1) + rectCover(number-2);
+        }
+        return mem[number];
     }
 };
