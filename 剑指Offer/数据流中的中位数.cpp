18fdd88ee5d887006bef94fb5a903ab1f6829718
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-28 16:12:46 +0800

    Update 数据流中的中位数.cpp
    用了插入排序。。。用了STL的方法和函数

--- 剑指Offer/数据流中的中位数.cpp
+++ 剑指Offer/数据流中的中位数.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
+    // 输入的时候采用 插入排序，利用了lower_bound函数
+    // 用了数组的insert方法，第一个参数是位置，第二个参数是 插入的值
     #define SCD static_cast<double>
     vector<int> v;
     void Insert(int num) {
-        v.push_back(num);
+        if(v.empty()) v.push_back(num);
+        else 
+        {
+            auto it = lower_bound(v.begin(), v.end(), num);
+            v.insert(it, num);
+        }
     }
 
     double GetMedian() { 
-        sort(v.begin(),v.end());
         int sz = v.size();
         if(sz&1)  // 判断是不是奇数
         {
