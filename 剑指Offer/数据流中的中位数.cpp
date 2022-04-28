322a53a48f0098e8b907027182757679db97db53
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-28 17:18:07 +0800

    Update 数据流中的中位数.cpp
    大顶堆和小顶堆的应用，牛逼

--- 剑指Offer/数据流中的中位数.cpp
+++ 剑指Offer/数据流中的中位数.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
-    // 输入的时候采用 插入排序，利用了lower_bound函数
-    // 用了数组的insert方法，第一个参数是位置，第二个参数是 插入的值
+    // 用了堆数据结构（priority_queue）来实现
+    
     #define SCD static_cast<double>
-    vector<int> v;
+    priority_queue<int> min_q;  // 默认是大顶堆
+    priority_queue<int,vector<int>, greater<int>> max_q; // 这就是小顶堆
+    
     void Insert(int num) {
-        if(v.empty()) v.push_back(num);
-        else 
+        min_q.push(num);// 试图加入到大顶推
+        
+        max_q.push(min_q.top());// 之所以先进大顶堆，就是因为可以将大的数放到右边的小顶堆
+        min_q.pop();
+        
+        // 平衡两个堆 
+        if(min_q.size()<max_q.size())  // 这里也一样，就是右边多了，就扔到左边，也是仍小的到左边
         {
-            auto it = lower_bound(v.begin(), v.end(), num);
-            v.insert(it, num);
+            min_q.push(max_q.top());
+            max_q.pop();
         }
     }
 
     double GetMedian() { 
-        int sz = v.size();
-        if(sz&1)  // 判断是不是奇数
-        {
-            return SCD(v[sz>>1]);
-        }
-        else 
-        {
-            return SCD((v[sz>>1]+v[(sz-1)>>1]))/2; // 这里要先SCD再除哦！不然就是取整了
-        }
+        /*
+        假设[0 ... median - 1]的长度为l_len, [median + 1 ... arr.sise() - 1]的长度为 r_len.
+            1.如果l_len == r_len + 1, 说明，中位数是左边数据结构的最大值
+            2.如果l_len + 1 == r_len, 说明，中位数是右边数据结构的最小值
+            3.如果l_len == r_len, 说明，中位数是左边数据结构的最大值与右边数据结构的最小值的平均值。
+        */
+        
+        return min_q.size()>max_q.size() ? SCD(min_q.top()) : SCD(min_q.top()+max_q.top())/2;
     }
 
 };
