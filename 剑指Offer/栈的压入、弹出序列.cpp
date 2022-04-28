44bdbab734506008dd982a29bd142eecb5f1bfb6
Swinie-Xu <67540892+Swinie-Xu@users.noreply.github.com>
2022-04-28 10:32:25 +0800

    Update 栈的压入、弹出序列.cpp
    感觉这个想法牛逼了啊！！！

--- 剑指Offer/栈的压入、弹出序列.cpp
+++ 剑指Offer/栈的压入、弹出序列.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     bool IsPopOrder(vector<int> pushV,vector<int> popV) {
-        // 照着解答自己没写出来，看着参考代码写的
-        // 感觉早上逻辑不清楚啊，咋回事
-        int n = pushV.size();
-        stack<int> s;//辅助栈
-        int j = 0;//遍历入栈的下标
-        for(int i=0;i<n;i++)//遍历出栈的数组
+        // 原地栈（扩展思路）
+        // 感觉这个厉害啊！！！
+        
+        
+        //表示栈空间的大小，初始化为0
+        int n=0;
+        //出栈序列的下标
+        int j=0;
+        //对于每个待入栈的元素
+        for(auto num:pushV)
         {
-            //入栈：栈为空或者栈顶不等于出栈数组
-            while(j<n && (s.empty() || s.top()!=popV[i]))
+            //加入栈顶
+            pushV[n] = num;
+            //当栈不为空且栈顶等于当前出栈序列
+            while(n>=0 && pushV[n] == popV[j])
             {
-                s.push(pushV[j]);
                 j++;
+                n--;
             }
-            // 自己写的时候一直找不到出口，其实只要没办法
-            // 按照特定顺序走完，就可以判断false了！
-            if(s.top() == popV[i]) s.pop(); //栈顶等于出栈数组
-            else return false;//不匹配序列
+            n++;
         }
-        return true;
+        //最后的栈是否为空
+        return n == 0;
     }
 };
